d91e76db43a2bc14db6afc6faad7886f376551ec
tuancosac <oituanday444@gmail.com>
2026-05-22 12:54:18 +0700

    Merge branch integrate

--- server/src/exercise/exercise.controller.ts
+++ server/src/exercise/exercise.controller.ts
@@ -1,9 +1,57 @@
-import { Controller, Get, Body, Param } from '@nestjs/common';
+import { Controller, Get, Body, Param, Post } from '@nestjs/common';
 import { ExerciseService } from './exercise.service';
+import { CodeExecutionService } from './code-execution/code-execution.service';
+import { InternalServerErrorException } from '@nestjs/common';
+
+interface RunCodeDto {
+  language: string;
+  code: string;
+  input: string;
+  exerciseId: string;
+}
 
 @Controller('exercise')
 export class ExerciseController {
-  constructor(private readonly exerciseService: ExerciseService) {}
+  constructor(
+    private readonly exerciseService: ExerciseService,
+    private readonly codeExecutionService: CodeExecutionService,
+  ) {}
+
+  // Trong exercise.controller.ts
+  @Post('run')
+  async run(@Body() body: RunCodeDto) {
+    // 1. Tìm exercise
+    const exercise = await this.exerciseService.findById(body.exerciseId);
+    if (!exercise) throw new InternalServerErrorException('Exercise not found');
+
+    // 2. Chạy code
+    const result = await this.codeExecutionService.runCode(
+      body.language,
+      body.code,
+      body.input,
+    );
+
+    // 3. Xử lý Output: Loại bỏ tất cả khoảng trắng thừa
+    const actualOutput = result.stdout
+      ? result.stdout.trim().replace(/\s+/g, '')
+      : '';
+
+    // 4. Lấy Expected Output từ DB và cũng xử lý sạch
+    const testCases = (exercise as any).testCases;
+    const expectedOutput =
+      testCases.length > 0
+        ? testCases[0].expectedOutput.trim().replace(/\s+/g, '')
+        : '';
+console.log("DEBUG: Nội dung biến result là:", JSON.stringify(result));
+    // 5. So sánh
+    return {
+      success: actualOutput === expectedOutput,
+      actual: actualOutput,
+      expected: expectedOutput,
+    runtime: (result as any).executionTime ? `${(result as any).executionTime}ms` : '0ms',
+  memory: (result as any).memoryUsed ? `${Math.round((result as any).memoryUsed / 1024)} MB` : '0 MB'
+    };
+  }
 
   @Get(':slug/:difficulty')
   findOne(
--- server/src/exercise/exercise.module.ts
+++ server/src/exercise/exercise.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ExerciseService } from './exercise.service';
 import { ExerciseController } from './exercise.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { CodeExecutionService } from './code-execution/code-execution.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, HttpModule],
   controllers: [ExerciseController],
-  providers: [ExerciseService],
+  providers: [ExerciseService, CodeExecutionService],
 })
 export class ExerciseModule {}
